3a123eabf3062360e08585e1fdf0b5cdc73b4e2d
Mabualzait <m.abualzait@gmail.com>
2020-04-06 09:44:58 +0300

    added dependencies

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/abualzait/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/abualzait/Movies/flutter/Flutter-Databse-SQFLite"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/abualzait/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
