fccad5a8a4d667a5849b2611f5d7cc911c3877af
Mabualzait <m.abualzait@gmail.com>
2020-04-16 19:20:17 +0300

    link with UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:workin_with_database/models/user.dart';
 
 List _users;
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   var db = new DatabaseHelper();
   int savedUserId = await db.saveUser(new User("Sameer","123"));
 //  print(savedUserId);
@@ -26,6 +27,22 @@ class Home extends StatelessWidget {
         title: new Text('Database'),
         centerTitle: true,
         backgroundColor: Colors.teal,
+      ),
+      body: new ListView.builder(
+          itemCount: _users.length,
+          itemBuilder: (_,int position){
+            return new Card(
+              color: Colors.white,
+              elevation: 2.0,
+              child: new ListTile(
+                leading: new CircleAvatar(
+                  child: new Text("${User.fromMap(_users[position]).username.substring(0,1)}"),
+                ),
+                title: new Text("User: ${User.fromMap(_users[position]).username}"),
+                subtitle: new Text("id: ${User.fromMap(_users[position]).id}"),
+              ),
+            );
+          }
       ),
     );
   }
